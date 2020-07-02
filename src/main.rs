4c1d43d38f3d6f12cf734e6581a5220c76d9b8d8
dougfort <doug.fort@gmail.com>
2020-07-02 11:20:35 -0500

    The Rocket 'raw_upload' example.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[cfg(test)]
+mod tests;
+
+use rocket::{response::Debug, Data};
+use std::{env, io};
+
+#[post("/upload", format = "plain", data = "<data>")]
+fn upload(data: Data) -> Result<String, Debug<io::Error>> {
+    data.stream_to_file(env::temp_dir().join("upload.txt"))
+        .map(|n| n.to_string())
+        .map_err(Debug)
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Upload your text files by POSTing them to /upload."
+}
+
+fn rocket() -> rocket::Rocket {
+    rocket::ignite().mount("/", routes![index, upload])
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket().launch();
 }
