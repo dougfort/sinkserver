e01ca32aeb6f87cd0c14600acf47bc12e4805c29
dougfort <doug.fort@gmail.com>
2020-07-04 09:53:09 -0500

    added headers and size computation

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,34 @@
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use futures::StreamExt;
-use log::info;
+use http::header;
+use log::debug;
 use std::io::Write;
 
 mod hashwriter;
 use hashwriter::HashWriter;
 
 async fn hello(req: HttpRequest) -> impl Responder {
-    info!("REQ: {:?}", req);
+    debug!("REQ: {:?}", req);
     "Hello world!"
 }
 
 async fn upload(mut body: web::Payload) -> Result<HttpResponse, Error> {
     let mut h = HashWriter::new();
+    let mut size: u64 = 0; 
     while let Some(item) = body.next().await {
-        h.write_all(&item?)?;
+        size += h.write(&item?)? as u64;
     }
     let digest = h.close();
 
-    info!("Digest = {:x?}", digest.as_slice());
-    Ok(HttpResponse::Ok().body(digest))
+    debug!("size = {}; Digest = {:x?}", size, digest.as_slice());
+    Ok(HttpResponse::Ok().header(header::CONTENT_TYPE, "application/octet-stream").body(digest))
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var(
         "RUST_LOG",
-        "actix_server=info,actix_web=info,sinkserver=info",
+        "actix_server=info,actix_web=info,sinkserver=debug",
     );
     env_logger::init();
 
