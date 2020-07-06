c3f9fa09619bebb7ef4d48777bcd0e93404bd555
dougfort <doug.fort@gmail.com>
2020-07-06 17:13:39 -0500

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -15,15 +15,21 @@ async fn hello(req: HttpRequest) -> impl Responder {
 
 async fn upload(mut body: web::Payload) -> Result<HttpResponse, Error> {
     let mut h = HashWriter::new();
-    let mut size: u64 = 0; 
+    let mut size: u64 = 0;
     while let Some(item) = body.next().await {
         size += h.write(&item?)? as u64;
     }
     let digest = h.close();
     let digest_as_slice = digest.as_slice();
 
-    debug!("size = {}; Digest = {:?}", size, hex::encode(digest_as_slice));
-    Ok(HttpResponse::Ok().header(header::CONTENT_TYPE, "application/octet-stream").body(digest))
+    debug!(
+        "size = {}; Digest = {:?}",
+        size,
+        hex::encode(digest_as_slice)
+    );
+    Ok(HttpResponse::Ok()
+        .header(header::CONTENT_TYPE, "application/octet-stream")
+        .body(digest))
 }
 
 #[actix_rt::main]
@@ -33,7 +39,7 @@ async fn main() -> std::io::Result<()> {
         "actix_server=info,actix_web=info,sinkserver=debug",
     );
     env_logger::init();
-    
+
     let address = construct_address();
 
     HttpServer::new(|| {
@@ -49,11 +55,11 @@ async fn main() -> std::io::Result<()> {
 fn construct_address() -> String {
     let host = match env::var_os("SINKSERVER_HOST") {
         Some(val) => val.into_string().unwrap(),
-        None => "0.0.0.0".to_string()
+        None => "0.0.0.0".to_string(),
     };
     let port = match env::var_os("SINKSERVER_PORT") {
         Some(val) => val.into_string().unwrap(),
-        None => "3000".to_string()
+        None => "3000".to_string(),
     };
     format!("{}:{}", host, port)
 }
